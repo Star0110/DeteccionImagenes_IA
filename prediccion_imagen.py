e831ec2d43832a536d5608eaf5049745fb3ffca0
luisbutnotommoo <ac32948139@gmail.com>
2025-05-24 19:47:28 -0600

    Más imagenes

--- prediccion_imagen.py
+++ prediccion_imagen.py
@@ -1,31 +1,46 @@
 from ultralytics import YOLO
 import os
+from collections import Counter
 
 # Cargamos el modelo nano
 model = YOLO("yolo11n.pt")
 
-#Definimos una lista con las clases que queremos encontrar
+# Definimos una lista con las clases que queremos encontrar
 vehicle_classes = ["car", "truck", "bus", "motorcycle", "bicycle"]
 
-
 def predict_image(image_path):
-    # definimos nombre de archivo
-    filename=os.path.basename(image_path)
-    filename.split(".")[0]
-    filename+="_prediction.jpg"
-    results=model(image_path)
+    # Definimos nombre de archivo de salida
+    filename = os.path.basename(image_path)
+    filename = filename.split(".")[0] + "_prediction.jpg"
 
+    # Ejecutamos el modelo sobre la imagen
+    results = model(image_path)
+
+    # Obtenemos las clases detectadas (índices -> nombres)
     detections = results[0].boxes.cls.cpu().numpy()
     detected_classes = [model.names[int(i)] for i in detections]
 
+    # Mostrar y guardar imagen con predicciones
     results[0].show()
     results[0].save(filename=filename)
 
-    flag = any(class_d in detected_classes for class_d in vehicle_classes)
+    # Contamos cuántas veces aparece cada clase
+    count_vehicle = Counter(detected_classes)
+
+    # Verificamos si hay algún vehículo
+    flag = any(clase in detected_classes for clase in vehicle_classes)
 
     if flag:
-       print(f"Se detectaron las clases: {detected_classes}")
+        print(f"\n\nVehículos detectados en la imagen:")
+        for type in vehicle_classes:
+            if type in count_vehicle:
+                print(f" - {type}: {count_vehicle[type]}")
+
+        total = sum(count_vehicle[type] for type in vehicle_classes if type in count_vehicle)
+        print(f"Total de vehículos: {total}")
+
     else:
-        print("La imagen no muestra vehiculos de ningun tipo")
+        print("La imagen no muestra vehículos de ningún tipo.")
+
 
    
